3b1d15420f90374f5b0ce88748747e9df0fd3a9d
KhaetbekIT <khaetbek@internet.ru>
2026-07-15 15:40:18 +0500

    feat: добавлены новые домашние задания в моковые данные

--- src/mocks/homeworks.mock.ts
+++ src/mocks/homeworks.mock.ts
@@ -16,4 +16,20 @@ export const HOMEWORK_OPTIONS: HomeworkOption[] = [
 		id: 3,
 		title: "Homework #3 - HTML & CSS (Практика по верстке)",
 	},
+	{
+		id: 4,
+		title: "Homework #4 - Figma (Практика Uzum)",
+	},
+	{
+		id: 5,
+		title: "Homework #5 - Watch (Практика с макетом)",
+	},
+	{
+		id: 6,
+		title: "Homework #6 - Карточка товара (Практика с макетом)",
+	},
+	{
+		id: 7,
+		title: "Homework #7 - Flex (Практика с макетом)",
+	},
 ];
